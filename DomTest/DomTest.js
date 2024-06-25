85adaf369a464f6d8b664d14378428ce1b8925ee
matbudimir <dotbudi@gmail.com>
2024-06-25 14:03:16 +0200

    upload

--- DomTest/DomTest.js
+++ DomTest/DomTest.js
@@ -8,9 +8,27 @@ newSpan.textContent = "nice day!";
 document.body.appendChild(newSpan);
 newSpan.style.backgroundColor = "";
 console.log(newSpan);
+//Array
+let bodyContent = [
+    "REDRUM", "CocaCola", "Code1", "ZeroSugar", "Goblin", "Cthulhu"
+];
 //Loops
 let i = 0;
 do {
     i++;
-    console.log(i);
-} while (i < 10);
+    let newSpan = document.createElement("span");
+    //Random Text
+    const randomText = bodyContent[Math.floor(Math.random() * bodyContent.length)];
+    newSpan.textContent = randomText;
+    //Random Color
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    newSpan.style.color = "#" + randomColor;
+    //Random Position
+    newSpan.style.position = "absolute";
+    newSpan.style.left = Math.random() * window.innerWidth + "px";
+    newSpan.style.top = Math.random() * window.innerHeight + "px";
+    //Random Size
+    newSpan.style.fontSize = Math.random() * window.innerWidth * 0.3 + "px";
+    //Append New Span
+    document.body.appendChild(newSpan);
+} while (i < 50);
--- DomTest/DomTest.ts
+++ DomTest/DomTest.ts
@@ -12,12 +12,37 @@ newSpan.style.backgroundColor = "";
 
 console.log(newSpan);
 
-//Loops
 
+
+//Array
+let bodyContent: string[] = [
+    "REDRUM", "CocaCola", "Code1", "ZeroSugar", "Goblin", "Cthulhu"
+];
+
+
+//Loops
 let i = 0;
 
 do {
     i++;
-    console.log(i);
+    let newSpan: HTMLSpanElement = document.createElement("span");
     
-} while (i < 10)
+    //Random Text
+    const randomText = bodyContent[Math.floor(Math.random() * bodyContent.length)];
+    newSpan.textContent = randomText;
+
+    //Random Color
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    newSpan.style.color = "#" + randomColor;
+
+    //Random Position
+    newSpan.style.position = "absolute";
+    newSpan.style.left = Math.random() * window.innerWidth + "px";
+    newSpan.style.top = Math.random() * window.innerHeight + "px";
+
+    //Random Size
+    newSpan.style.fontSize = Math.random() * window.innerWidth * 0.3 + "px";
+
+    //Append New Span
+    document.body.appendChild(newSpan);
+} while (i < 50)
