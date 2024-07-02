63a0d3102a2c719ae7b1254cc12063c65688d956
matbudimir <dotbudi@gmail.com>
2024-07-02 10:34:59 +0200

    clouds

--- CanvasPainting/paint.js
+++ CanvasPainting/paint.js
@@ -1,36 +1,6 @@
 "use strict";
 const canvas = document.getElementsByTagName("canvas")[0];
 const ctx = canvas.getContext("2d");
-// Background
-let k = 0;
-do {
-    k++;
-    let pathBG = new Path2D();
-    let size = Math.random() * 75 + 720;
-    pathBG.ellipse(Math.random() * 1900, Math.random() * 100 + 1350, size, size, Math.PI / 4, 0, 2 * Math.PI);
-    ctx.fillStyle = "#b2beb5";
-    ctx.fill(pathBG);
-} while (k < 10);
-// Middleground
-let j = 0;
-do {
-    j++;
-    let pathMG = new Path2D();
-    let size = Math.random() * 75 + 420;
-    pathMG.ellipse(Math.random() * 1900, Math.random() * 100 + 1150, size, size, Math.PI / 4, 0, 2 * Math.PI);
-    ctx.fillStyle = "#7bb661";
-    ctx.fill(pathMG);
-} while (j < 20);
-// Foreground
-let i = 0;
-do {
-    i++;
-    let pathFG = new Path2D();
-    let size = Math.random() * 75 + 120;
-    pathFG.ellipse(Math.random() * 1900, Math.random() * 100 + 1000, size, size, Math.PI / 4, 0, 2 * Math.PI);
-    ctx.fillStyle = "#4f7942";
-    ctx.fill(pathFG);
-} while (i < 120);
 // Sun
 let s = 0;
 do {
@@ -41,6 +11,101 @@ do {
     ctx.fillStyle = "#ffffe0";
     ctx.fill(pathSun);
 } while (s < 1);
+// Background
+let k = 0;
+do {
+    k++;
+    let bgR = Math.random() * 15 + 155;
+    let bgG = Math.random() * 15 + 195;
+    let bgB = Math.random() * 15 + 175;
+    let pathBG = new Path2D();
+    let size = Math.random() * 75 + 720;
+    pathBG.ellipse(Math.random() * 1900, Math.random() * 100 + 1350, size, size, Math.PI / 4, 0, 2 * Math.PI);
+    ctx.fillStyle = "rgb(" + bgR + ", " + bgG + "," + bgB + ")";
+    ctx.fill(pathBG);
+} while (k < 10);
+let cloudsBG = [];
+for (let wbg = 0; wbg < 8; wbg++) {
+    cloudsBG.push({
+        positionX: Math.random() * 1920,
+        positionY: Math.random() * 50 + 150,
+        scaleX: Math.random() * 1 + 2,
+        scaleY: Math.random() * 1 + 5,
+        cloudD: Math.random() * 25 + 75,
+        color: Math.random() * 25 + 175,
+    });
+}
+function drawCloudBG() {
+    for (let cbg = 0; cbg < cloudsBG.length; cbg++) {
+        let pathCloudBG = new Path2D();
+        pathCloudBG.ellipse(cloudsBG[cbg].positionX, cloudsBG[cbg].positionY, 10 * cloudsBG[cbg].scaleX, 10 * cloudsBG[cbg].scaleY, Math.PI / 2, 0, 2 * Math.PI);
+        ctx.fillStyle = "rgb(" + cloudsBG[cbg].color + ", " + cloudsBG[cbg].color + "," + cloudsBG[cbg].color + ")";
+        ctx.fill(pathCloudBG);
+        for (let clbg = 0; clbg < cloudsBG[cbg].cloudD; clbg++) {
+            let pathCDBG = new Path2D;
+            pathCDBG.ellipse(cloudsBG[cbg].positionX + (Math.random() * 100) - (Math.random() * 100), cloudsBG[cbg].positionY + (Math.random() * 50) - (Math.random() * 35), 10 * cloudsBG[cbg].scaleX, 10 * cloudsBG[cbg].scaleY, Math.PI / 2, 0, 2 * Math.PI);
+            ctx.fillStyle = "rgb(" + cloudsBG[cbg].color + ", " + cloudsBG[cbg].color + "," + cloudsBG[cbg].color + ")";
+            ctx.fill(pathCDBG);
+        }
+    }
+}
+drawCloudBG();
+// Middleground
+let j = 0;
+do {
+    j++;
+    let mgR = Math.random() * 20 + 85;
+    let mgG = Math.random() * 25 + 155;
+    let mgB = Math.random() * 45 + 75;
+    let pathMG = new Path2D();
+    let size = Math.random() * 75 + 420;
+    pathMG.ellipse(Math.random() * 1900, Math.random() * 100 + 1150, size, size, Math.PI / 4, 0, 2 * Math.PI);
+    ctx.fillStyle = "rgb(" + mgR + ", " + mgG + "," + mgB + ")";
+    ctx.fill(pathMG);
+} while (j < 20);
+let treesMG = [];
+for (let b = 0; b < 12; b++) {
+    treesMG.push({
+        positionX: Math.random() * 1920,
+        positionY: Math.random() * 150 + 400,
+        scaleX: Math.random() * 1 + 1,
+        scaleY: Math.random() * 1 + 1,
+        leaves: Math.random() * 25 + 35,
+        color: "#80755a",
+        colorR: Math.random() * 20 + 75,
+        colorG: Math.random() * 25 + 145,
+        colorB: Math.random() * 45 + 65,
+        hasLeaves: true,
+    });
+}
+function drawTreeMG() {
+    for (let t = 0; t < treesMG.length; t++) {
+        let pathTreeMG = new Path2D();
+        pathTreeMG.rect(treesMG[t].positionX, treesMG[t].positionY, 10 * treesMG[t].scaleX, 500 * treesMG[t].scaleY);
+        ctx.fillStyle = treesMG[1].color;
+        ctx.fill(pathTreeMG);
+        for (let l = 0; l < treesMG[t].leaves; l++) {
+            let pathLeafMG = new Path2D;
+            pathLeafMG.ellipse(treesMG[t].positionX + (Math.random() * 70) - (Math.random() * 70), treesMG[t].positionY + (Math.random() * 100) - (Math.random() * 70), Math.random() * 15 + 25, Math.random() * 15 + 25, Math.PI / 2, 0, 2 * Math.PI);
+            ctx.fillStyle = "rgb(" + treesMG[t].colorR + ", " + treesMG[t].colorG + "," + treesMG[t].colorB + ")";
+            ctx.fill(pathLeafMG);
+        }
+    }
+}
+drawTreeMG();
+// Foreground
+let i = 0;
+do {
+    i++;
+    let fgR = Math.random() * 25 + 55;
+    let fgG = Math.random() * 45 + 95;
+    let fgB = Math.random() * 75 + 15;
+    let pathFG = new Path2D();
+    let size = Math.random() * 75 + 120;
+    pathFG.ellipse(Math.random() * 1900, Math.random() * 100 + 1000, size, size, Math.PI / 4, 0, 2 * Math.PI);
+    ctx.fillStyle = "rgb(" + fgR + ", " + fgG + "," + fgB + ")";
+    ctx.fill(pathFG);
+} while (i < 120);
 let clouds = [];
 for (let w = 0; w < 3; w++) {
     clouds.push({
@@ -67,17 +132,17 @@ function drawCloud() {
     }
 }
 let trees = [];
-for (let b = 0; b < 9; b++) {
+for (let b = 0; b < 5; b++) {
     trees.push({
         positionX: Math.random() * 1920,
-        positionY: Math.random() * 200 + 400,
+        positionY: Math.random() * 200 + 250,
         scaleX: Math.random() * 1 + 2,
-        scaleY: Math.random() * 1 + 1,
-        leaves: Math.random() * 25 + 35,
-        color: "#80755a",
-        colorR: Math.random() * 25 + 55,
-        colorG: Math.random() * 45 + 95,
-        colorB: Math.random() * 75 + 15,
+        scaleY: Math.random() * 1 + 1.2,
+        leaves: Math.random() * 25 + 45,
+        color: "#654321",
+        colorR: Math.random() * 25 + 35,
+        colorG: Math.random() * 45 + 75,
+        colorB: Math.random() * 65 + 5,
         hasLeaves: true,
     });
 }
@@ -89,7 +154,7 @@ function drawTree() {
         ctx.fill(pathTree);
         for (let l = 0; l < trees[t].leaves; l++) {
             let pathLeaf = new Path2D;
-            pathLeaf.ellipse(trees[t].positionX + (Math.random() * 100) - (Math.random() * 100), trees[t].positionY + (Math.random() * 150) - (Math.random() * 100), Math.random() * 50 + 25, Math.random() * 50 + 25, Math.PI / 2, 0, 2 * Math.PI);
+            pathLeaf.ellipse(trees[t].positionX + (Math.random() * 100) - (Math.random() * 100), trees[t].positionY + (Math.random() * 250) - (Math.random() * 100), Math.random() * 50 + 25, Math.random() * 50 + 25, Math.PI / 2, 0, 2 * Math.PI);
             ctx.fillStyle = "rgb(" + trees[t].colorR + ", " + trees[t].colorG + "," + trees[t].colorB + ")";
             ctx.fill(pathLeaf);
         }
--- CanvasPainting/paint.ts
+++ CanvasPainting/paint.ts
@@ -1,40 +1,6 @@
 const canvas: HTMLCanvasElement = document.getElementsByTagName("canvas")[0];
 const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
-
-// Background
-let k = 0;
-do {
-    k++;
-    let pathBG = new Path2D();
-    let size = Math.random() * 75 + 720;
-    pathBG.ellipse(Math.random() * 1900, Math.random() * 100 + 1350, size, size, Math.PI / 4, 0, 2 * Math.PI);
-    ctx.fillStyle = "#b2beb5";
-    ctx.fill(pathBG);
-} while (k < 10);
-
-
-// Middleground
-let j = 0;
-do {
-    j++;
-    let pathMG = new Path2D();
-    let size = Math.random() * 75 + 420;
-    pathMG.ellipse(Math.random() * 1900, Math.random() * 100 + 1150, size, size, Math.PI / 4, 0, 2 * Math.PI);
-    ctx.fillStyle = "#7bb661";
-    ctx.fill(pathMG);
-} while (j < 20);
-
-
-// Foreground
-let i = 0;
-do {
-    i++;
-    let pathFG = new Path2D();
-    let size = Math.random() * 75 + 120;
-    pathFG.ellipse(Math.random() * 1900, Math.random() * 100 + 1000, size, size, Math.PI / 4, 0, 2 * Math.PI);
-    ctx.fillStyle = "#4f7942";
-    ctx.fill(pathFG);
-} while (i < 120);
+type RGB = `rgb(${number}, ${number}, ${number})`;
 
 // Sun
 let s = 0;
@@ -47,7 +13,140 @@ do {
     ctx.fill(pathSun);
 } while (s < 1);
 
-// Clouds
+// Background
+let k = 0;
+do {
+    k++;
+    let bgR = Math.random() * 15 + 155;
+    let bgG = Math.random() * 15 + 195;
+    let bgB = Math.random() * 15 + 175;
+    let pathBG = new Path2D();
+    let size = Math.random() * 75 + 720;
+    pathBG.ellipse(Math.random() * 1900, Math.random() * 100 + 1350, size, size, Math.PI / 4, 0, 2 * Math.PI);
+    ctx.fillStyle = "rgb(" + bgR + ", " + bgG + "," + bgB + ")";
+    ctx.fill(pathBG);
+} while (k < 10);
+
+// Clouds Background
+interface CloudAttributesBG {
+    positionX: number;
+    positionY: number;
+    scaleX: number;
+    scaleY: number;
+    cloudD: number;
+    color: number;
+}
+
+let cloudsBG: CloudAttributesBG[] = [];
+
+for (let wbg: number = 0; wbg < 8; wbg++) {
+    cloudsBG.push({
+        positionX: Math.random() * 1920,
+        positionY: Math.random() * 50 + 150,
+        scaleX: Math.random() * 1 + 2,
+        scaleY: Math.random() * 1 + 5,
+        cloudD: Math.random() * 25 + 75,
+        color: Math.random() * 25 + 175,
+    });
+}
+
+function drawCloudBG(): void {
+    for (let cbg: number = 0; cbg < cloudsBG.length; cbg++) {
+        let pathCloudBG = new Path2D();
+        pathCloudBG.ellipse(cloudsBG[cbg].positionX, cloudsBG[cbg].positionY, 10 * cloudsBG[cbg].scaleX, 10 * cloudsBG[cbg].scaleY, Math.PI / 2, 0, 2 * Math.PI);
+        ctx.fillStyle = "rgb(" + cloudsBG[cbg].color + ", " + cloudsBG[cbg].color + "," + cloudsBG[cbg].color + ")";
+        ctx.fill(pathCloudBG);
+
+        for (let clbg: number = 0; clbg < cloudsBG[cbg].cloudD; clbg++) {
+            let pathCDBG = new Path2D;
+            pathCDBG.ellipse(cloudsBG[cbg].positionX + (Math.random() * 100) - (Math.random() * 100), cloudsBG[cbg].positionY + (Math.random() * 50) - (Math.random() * 35), 10 * cloudsBG[cbg].scaleX, 10 * cloudsBG[cbg].scaleY, Math.PI / 2, 0, 2 * Math.PI);
+            ctx.fillStyle = "rgb(" + cloudsBG[cbg].color + ", " + cloudsBG[cbg].color + "," + cloudsBG[cbg].color + ")";
+            ctx.fill(pathCDBG);
+        }
+    }
+}
+
+drawCloudBG();
+
+// Middleground
+let j = 0;
+do {
+    j++;
+    let mgR = Math.random() * 20 + 85;
+    let mgG = Math.random() * 25 + 155;
+    let mgB = Math.random() * 45 + 75;
+    let pathMG = new Path2D();
+    let size = Math.random() * 75 + 420;
+    pathMG.ellipse(Math.random() * 1900, Math.random() * 100 + 1150, size, size, Math.PI / 4, 0, 2 * Math.PI);
+    ctx.fillStyle = "rgb(" + mgR + ", " + mgG + "," + mgB + ")";
+    ctx.fill(pathMG);
+} while (j < 20);
+
+
+
+interface TreeAttributesMG {
+    positionX: number;
+    positionY: number;
+    scaleX: number;
+    scaleY: number;
+    leaves: number;
+    color: string;
+    colorR: number;
+    colorG: number;
+    colorB: number;
+    hasLeaves: boolean;
+}
+
+let treesMG: TreeAttributesMG[] = [];
+
+for (let b: number = 0; b < 12; b++) {
+    treesMG.push({
+        positionX: Math.random() * 1920,
+        positionY: Math.random() * 150 + 400,
+        scaleX: Math.random() * 1 + 1,
+        scaleY: Math.random() * 1 + 1,
+        leaves: Math.random() * 25 + 35,
+        color: "#80755a",
+        colorR: Math.random() * 20 + 75,
+        colorG: Math.random() * 25 + 145,
+        colorB: Math.random() * 45 + 65,
+        hasLeaves: true,
+    });
+}
+
+function drawTreeMG(): void {
+    for (let t: number = 0; t < treesMG.length; t++) {
+        let pathTreeMG = new Path2D();
+        pathTreeMG.rect(treesMG[t].positionX, treesMG[t].positionY, 10 * treesMG[t].scaleX, 500 * treesMG[t].scaleY);
+        ctx.fillStyle = treesMG[1].color;
+        ctx.fill(pathTreeMG);
+
+        for (let l: number = 0; l < treesMG[t].leaves; l++) {
+            let pathLeafMG = new Path2D;
+            pathLeafMG.ellipse(treesMG[t].positionX + (Math.random() * 70) - (Math.random() * 70), treesMG[t].positionY + (Math.random() * 100) - (Math.random() * 70), Math.random() * 15 + 25, Math.random() * 15 + 25, Math.PI / 2, 0, 2 * Math.PI)
+            ctx.fillStyle = "rgb(" + treesMG[t].colorR + ", " + treesMG[t].colorG + "," + treesMG[t].colorB + ")";
+            ctx.fill(pathLeafMG);
+        }
+    }
+}
+
+drawTreeMG();
+
+// Foreground
+let i = 0;
+do {
+    i++;
+    let fgR = Math.random() * 25 + 55;
+    let fgG = Math.random() * 45 + 95;
+    let fgB = Math.random() * 75 + 15;
+    let pathFG = new Path2D();
+    let size = Math.random() * 75 + 120;
+    pathFG.ellipse(Math.random() * 1900, Math.random() * 100 + 1000, size, size, Math.PI / 4, 0, 2 * Math.PI);
+    ctx.fillStyle = "rgb(" + fgR + ", " + fgG + "," + fgB + ")";
+    ctx.fill(pathFG);
+} while (i < 120);
+
+// Clouds Foreground
 interface CloudAttributes {
     positionX: number;
     positionY: number;
@@ -58,7 +157,6 @@ interface CloudAttributes {
 }
 
 let clouds: CloudAttributes[] = [];
-type RGB = `rgb(${number}, ${number}, ${number})`;
 
 for (let w: number = 0; w < 3; w++) {
     clouds.push({
@@ -102,17 +200,17 @@ interface TreeAttributes {
 
 let trees: TreeAttributes[] = [];
 
-for (let b: number = 0; b < 9; b++) {
+for (let b: number = 0; b < 5; b++) {
     trees.push({
         positionX: Math.random() * 1920,
-        positionY: Math.random() * 200 + 400,
+        positionY: Math.random() * 200 + 250,
         scaleX: Math.random() * 1 + 2,
-        scaleY: Math.random() * 1 + 1,
-        leaves: Math.random() * 25 + 35,
-        color: "#80755a",
-        colorR: Math.random() * 25 + 55,
-        colorG: Math.random() * 45 + 95,
-        colorB: Math.random() * 75 + 15,
+        scaleY: Math.random() * 1 + 1.2,
+        leaves: Math.random() * 25 + 45,
+        color: "#654321",
+        colorR: Math.random() * 25 + 35,
+        colorG: Math.random() * 45 + 75,
+        colorB: Math.random() * 65 + 5,
         hasLeaves: true,
     });
 }
@@ -126,7 +224,7 @@ function drawTree(): void {
 
         for (let l: number = 0; l < trees[t].leaves; l++) {
             let pathLeaf = new Path2D;
-            pathLeaf.ellipse(trees[t].positionX + (Math.random() * 100) - (Math.random() * 100), trees[t].positionY + (Math.random() * 150) - (Math.random() * 100), Math.random() * 50 + 25, Math.random() * 50 + 25, Math.PI / 2, 0, 2 * Math.PI)
+            pathLeaf.ellipse(trees[t].positionX + (Math.random() * 100) - (Math.random() * 100), trees[t].positionY + (Math.random() * 250) - (Math.random() * 100), Math.random() * 50 + 25, Math.random() * 50 + 25, Math.PI / 2, 0, 2 * Math.PI)
             ctx.fillStyle = "rgb(" + trees[t].colorR + ", " + trees[t].colorG + "," + trees[t].colorB + ")";
             ctx.fill(pathLeaf);
         }
